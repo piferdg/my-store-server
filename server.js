49b8bd6be25e27b3c323aef5dd198c0fdc0b0850
Danny Pifer <piferdg@gmail.com>
2019-08-27 16:52:59 -0600

    Installed jwt packages for auth and configured projected endpoint

--- server.js
+++ server.js
@@ -3,11 +3,35 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
+const jwt = require('express-jwt')
+const jwksRsa = require('jwks-rsa')
+
 
 const PORT = process.env.PORT || 4000
 const app = express()
 
-//Middleware
+// Set up Auth0 configuration
+const authConfig = {
+  domain: 'dev-gtjv596d.auth0.com',
+  audience: 'http://localhost:4000'
+}
+
+// Define middleware that validates incoming bearer tokens
+// using JWKS from YOUR_DOMAIN
+const checkJwt = jwt({
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://${authConfig.domain}/.well-known/jwks.json`
+  }),
+
+  audience: authConfig.audience,
+  issuer: `https://${authConfig.domain}/`,
+  algorithm: ['RS256']
+})
+
+//Middleware - to be use on the entire application
 app.use(cors())
 app.use(morgan('dev'))
 app.use(express.json())
@@ -91,6 +115,12 @@ app.post('/api/checkout', async (req, res, next) => {
   catch (error) {
     next(error)
   }
+})
+
+app.get('/api/external', checkJwt, (req, res) => {
+  res.json({
+    msg: 'Your Access Token was successfully validated!'
+  })
 })
 
 
